59be8447bc0a994837e58247a10706a992921a4d
nitinvaishna <96920957+nitinvaishna@users.noreply.github.com>
2022-04-11 15:45:39 +0530

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,18 @@
+//Working on Online portal
+import java.util.HashMap;
+
+class Hashing
+{
+	public static void main(String args[])
+	{
+		HashMap<String, Integer>map = new HashMap<>();
+		
+			map.put("Rohan", 24);
+			map.put("Kishan", 54);
+		
+		System.out.println( map.size());
+		System.out.println(map);
+	}
+}
+		
+			
